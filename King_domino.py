dc37f783060173152ee9f62f19582d398020ad78
DEADEYEJ <31163769+DEADEYEJ@users.noreply.github.com>
2025-09-10 13:01:20 +0200

    King domino - test branch

--- King_domino.py
+++ King_domino.py
@@ -7,7 +7,7 @@ def main():
     print("+-------------------------------+")
     print("| King Domino points calculator |")
     print("+-------------------------------+")
-    image_path = r"C:\Users\admin\Downloads\King Domino dataset\5.jpg"
+    image_path = r"C:\Users\Jared\Downloads\King Domino train\73.jpg"
     if not os.path.isfile(image_path):
         print("Image not found")
         return
@@ -32,21 +32,21 @@ def get_tiles(image):
 # Determine the type of terrain in a tile
 def get_terrain(tile):
     hsv_tile = cv.cvtColor(tile, cv.COLOR_BGR2HSV)
-    hue, saturation, value = np.mean(hsv_tile, axis=(0,1)) # Consider using median instead of mean
+    hue, saturation, value = np.median(hsv_tile, axis=(0,1)) # Consider using median instead of mean
     print(f"H: {hue}, S: {saturation}, V: {value}")
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 12 < hue < 36 and 222 < saturation < 264 and 125 < value < 211:
         return "Field"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 18 < hue < 68 and 73 < saturation < 216 and 24 < value < 73:
         return "Forest"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 95 < hue < 119 and 230 < saturation < 264 and 107 < value < 194:
         return "Lake"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 25 < hue < 54 and 189 < saturation < 257 and 64 < value < 168:
         return "Grassland"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 8 < hue < 33 and 65 < saturation < 176 and 72 < value < 128:
         return "Swamp"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 9 < hue < 34 and 54 < saturation < 129 and 21 < value < 74:
         return "Mine"
-    if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
+    if 11 < hue < 97 and 31 < saturation < 204 and 49 < value < 130:
         return "Home"
     return "Unknown"
 
